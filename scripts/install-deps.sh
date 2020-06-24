bf6e77e9032c1f27b7a562ea653a84ca79255f2d
M. George Hansen <mgeorge@technopolitica.com>
2020-06-24 10:32:37 -0700

    chore(ci): don't check of dir in cache

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,10 +2,8 @@
 
 VCPKG_DIR="$TRAVIS_BUILD_DIR/vcpkg"
 
-if [ ! -d ./vcpkg ]; then
-  git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "$VCPKG_DIR" &&
-    cd "$VCPKG_DIR" &&
-    ./bootstrap-vcpkg.sh
-fi
+git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "$VCPKG_DIR" &&
+  cd "$VCPKG_DIR" &&
+  ./bootstrap-vcpkg.sh
 
 "$VCPKG_DIR"/vcpkg install fmt glfw3 glbinding
