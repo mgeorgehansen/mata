4428f0ffe0d41ce526015f169f04ab872f866012
M. George Hansen <mgeorge@technopolitica.com>
2020-06-24 09:45:23 -0700

    chore(ci): setup travis

---
+++ scripts/install-deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ROOT_DIR=$(cwd)
+
+git clone https://github.com/Microsoft/vcpkg.git &&
+  cd vcpkg &&
+  ./bootstrap-vcpkg.sh &&
+  cd "$ROOT_DIR"
+
+apt-get install -y libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev &&
+  /opt/vcpkg/vcpkg install fmt glfw3 glbinding
