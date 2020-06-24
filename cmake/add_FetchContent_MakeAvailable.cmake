b37140f5e3018fe1f40f88637572a57fe39c43b7
M. George Hansen <mgeorge@technopolitica.com>
2020-06-24 09:10:12 -0700

    chore: add MPL-2 license headers & text

--- cmake/add_FetchContent_MakeAvailable.cmake
+++ cmake/add_FetchContent_MakeAvailable.cmake
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public License,
+# v. 2.0. If a copy of the MPL was not distributed with this file, You can
+# obtain one at https://mozilla.org/MPL/2.0/.
+
 if(${CMAKE_VERSION} VERSION_LESS 3.14)
   macro(FetchContent_MakeAvailable NAME)
     FetchContent_GetProperties(${NAME})
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,9 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 #include <mata/lib.hpp>
 
-TEST_CASE( "Quick check", "[main]" ) {
-    REQUIRE( 2.0 == 2.0 );
-}
+TEST_CASE("Quick check", "[main]") { REQUIRE(2.0 == 2.0); }
