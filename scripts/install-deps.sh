a1e1a5708fdf065c22a01d9a943ef1a5d3e5dedd
M. George Hansen <mgeorge@technopolitica.com>
2020-06-24 10:14:15 -0700

    chore(ci): move apt-get to travis config

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -5,7 +5,4 @@ ROOT_DIR=$(pwd)
 git clone https://github.com/Microsoft/vcpkg.git &&
   cd vcpkg &&
   ./bootstrap-vcpkg.sh &&
-  cd "$ROOT_DIR"
-
-apt-get install -y libxinerama-dev libxcursor-dev xorg-dev libglu1-mesa-dev &&
-  /opt/vcpkg/vcpkg install fmt glfw3 glbinding
+  vcpkg install fmt glfw3 glbinding
