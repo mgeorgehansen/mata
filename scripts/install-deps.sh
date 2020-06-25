a9e8bbbda5ed63553200c69651a9d771f33ddca6
M. George Hansen <mgeorge@technopolitica.com>
2020-06-24 18:07:02 -0700

    chore(ci): fix path to vcpkg cmake toolchain file

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -2,6 +2,7 @@
 
 VCPKG_DIR="$TRAVIS_BUILD_DIR/vcpkg"
 VCPKG="$VCPKG_DIR/vcpkg"
+export VCPKG_CMAKE_TOOLCHAIN="$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake"
 
 if [ ! -f "$VCPKG" ]; then
   git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "$VCPKG_DIR" &&
