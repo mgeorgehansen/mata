6ef1d2872ed67adb46ca3d018ac45e5b653b9881
M. George Hansen <mgeorge@technopolitica.com>
2020-06-25 22:20:11 -0700

    chore(ci): restore path after vcpkg checkout

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -5,9 +5,11 @@ VCPKG="$VCPKG_DIR/vcpkg"
 export VCPKG_CMAKE_TOOLCHAIN="$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake"
 
 if [ ! -f "$VCPKG" ]; then
+  cwd=$(pwd)
   git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "$VCPKG_DIR" &&
     cd "$VCPKG_DIR" &&
-    ./bootstrap-vcpkg.sh
+    ./bootstrap-vcpkg.sh &&
+    cd "$cwd"
 fi
 
 "$VCPKG" install fmt glfw3 glbinding
