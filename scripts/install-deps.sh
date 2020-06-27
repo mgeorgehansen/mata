2276abc5f84af0f711f3b241214561f0e675cf78
M. George Hansen <mgeorge@technopolitica.com>
2020-06-26 17:30:56 -0700

    chore(ci): add logging to install-deps

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 VCPKG_HOME="${VCPKG_HOME:-$HOME/vcpkg}"
+echo "$0: using VCPKG_HOME=${VCPKG_HOME}"
 VCPKG="$VCPKG_HOME/vcpkg"
 
 if [ ! -f "$VCPKG" ]; then
+  echo "$0: vcpkg not found at $VCPKG, fetching vcpkg and building..."
   cwd=$(pwd)
   git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "$VCPKG_DIR" &&
     cd "$VCPKG_HOME" &&
