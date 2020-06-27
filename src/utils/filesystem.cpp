dcce17d6a879daa029e5c3f7bd544c90decbc4d2
M. George Hansen <mgeorge@technopolitica.com>
2020-06-27 08:05:00 -0700

    fix: support execPath on windows

--- src/utils/filesystem.cpp
+++ src/utils/filesystem.cpp
@@ -4,8 +4,10 @@
 
 #include <exception>
 #include <filesystem>
-#ifdef __APPLE__
+#if defined(__APPLE__)
 #include <mach-o/dyld.h>
+#elif defined(_MSC_VER)
+#include <Windows.h>
 #else
 #include <limits.h>
 #include <unistd.h>
@@ -16,13 +18,37 @@
 namespace mata {
 
 [[nodiscard]] static std::filesystem::path execPath() {
-#if __APPLE__
-  // TODO: implement cross-platform.
+#if defined(__APPLE__)
   uint32_t size = 0;
   _NSGetExecutablePath(nullptr, &size);
   const auto buffer = new char[size];
   _NSGetExecutablePath(buffer, &size);
   return std::filesystem::canonical(buffer);
+#elif defined(_MSVC_VER)
+  std::vector<char> buf(1024, 0);
+  auto size = buf.size();
+  auto havePath = false;
+  auto shouldContinue = true;
+  // Keep trying to load the exec path into buffer, doubling the buffer size
+  // each time if the buffer is too small.
+  // TODO: limit number of resizes to prevent unbounded memory usage.
+  do {
+    auto result = GetModuleFileNameA(nullptr, &buf[0], size);
+    auto lastError = GetLastError();
+    if (result == 0) {
+      shouldContinue = false;
+    } else if (result < size) {
+      havePath = true;
+      shouldContinue = false;
+    } else if (result == size && (lastError == ERROR_INSUFFICIENT_BUFFER ||
+                                  lastError == ERROR_SUCCESS)) {
+      // Path is larger than our buffer, so double the buffer and try again.
+      size *= 2;
+      buf.resize(size);
+    } else {
+      shouldContinue = false;
+    }
+  } while (shouldContinue);
 #else
   char result[PATH_MAX];
   const ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
