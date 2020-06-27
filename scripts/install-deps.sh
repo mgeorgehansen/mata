81070c3c2d0e3168511d61e5d8e26b328045e4f4
M. George Hansen <mgeorge@technopolitica.com>
2020-06-26 17:34:44 -0700

    chore(ci): fix install-deps VCPKG_HOME bug

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -7,7 +7,7 @@ VCPKG="$VCPKG_HOME/vcpkg"
 if [ ! -f "$VCPKG" ]; then
   echo "$0: vcpkg not found at $VCPKG, fetching vcpkg and building..."
   cwd=$(pwd)
-  git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "$VCPKG_DIR" &&
+  git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "$VCPKG_HOME" &&
     cd "$VCPKG_HOME" &&
     ./bootstrap-vcpkg.sh &&
     cd "$cwd"
