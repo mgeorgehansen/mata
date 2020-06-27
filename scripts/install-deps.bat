f3a4bb12480ec699d606fb147d829c9d1b4f4eed
M. George Hansen <mgeorge@technopolitica.com>
2020-06-26 22:22:29 -0700

    chore(ci): handle cached half-built vcpkg

--- scripts/install-deps.bat
+++ scripts/install-deps.bat
@@ -6,13 +6,20 @@ if not defined VCPKG_HOME set "VCPKG_HOME=%USERPROFILE%\vcpkg"
 set VCPKG="%VCPKG_HOME%\vcpkg.exe"
 echo "using VCPKG_HOME=%VCPKG_HOME%"
 
-if not exist %VCPKG% (
-  echo "vcpkg not found at %VCPKG%, fetching vcpkg and building..."
-  set cwd="%cd%"
-  git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "%VCPKG_HOME%" || exit /b 1
-  cd "%VCPKG_HOME%"
-  bootstrap-vcpkg.bat || exit /b 1
-  cd "%cwd%"
+set cwd="%cd%"
+cd "%VCPKG_HOME%"
+
+git rev-parse --is-inside-work-tree
+if errorlevel 1 (
+  echo "vcpkg source not found, cloning vcpkg..."
+  git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git . || exit /b 1
 )
+
+if not exist %VCPKG% (
+  echo "vcpkg binary not found at %VCPKG%, building vcpkg..."
+  bootstrap-vcpkg.bat || exit /b 1
+)
+
+cd "%cwd%"
 
 "%VCPKG%" install fmt glfw3 glbinding
--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 VCPKG_HOME="${VCPKG_HOME:-$HOME/vcpkg}"
-echo "$0: using VCPKG_HOME=${VCPKG_HOME}"
 VCPKG="$VCPKG_HOME/vcpkg"
+echo "$0: using VCPKG_HOME=${VCPKG_HOME}"
+
+cwd=$(pwd)
+cd "$VCPKG_HOME"
+
+if ! git rev-parse --is-inside-work-tree &> /dev/null; then
+  echo "$0: vcpkg sources not found, cloning vcpkg repo..."
+  git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git . || exit 1
+fi
 
 if [ ! -f "$VCPKG" ]; then
-  echo "$0: vcpkg not found at $VCPKG, fetching vcpkg and building..."
-  cwd=$(pwd)
-  git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "$VCPKG_HOME" || exit 1
-  cd "$VCPKG_HOME"
+  echo "$0: vcpkg binary not found at $VCPKG, building..."
   ./bootstrap-vcpkg.sh || exit 1
-  cd "$cwd"
 fi
+
+cd "$cwd"
 
 "$VCPKG" install fmt glfw3 glbinding
