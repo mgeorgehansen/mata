e1708f6c48063f4905268511d23f71f1e3a3ff86
M. George Hansen <mgeorge@technopolitica.com>
2020-06-26 22:00:22 -0700

    chore(ci): fix more windows syntax errors (grr)

--- scripts/install-deps.bat
+++ scripts/install-deps.bat
@@ -7,11 +7,11 @@ set VCPKG="%VCPKG_HOME%\vcpkg.exe"
 echo "using VCPKG_HOME=%VCPKG_HOME%"
 
 if not exist %VCPKG% (
-  echo "vcpkg not found at $VCPKG, fetching vcpkg and building..."
+  echo "vcpkg not found at %VCPKG%, fetching vcpkg and building..."
   set cwd="%cd%"
   git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "%VCPKG_HOME%" || exit /b 1
   cd "%VCPKG_HOME%"
-  ./bootstrap-vcpkg.bat || exit /b 1
+  bootstrap-vcpkg.bat || exit /b 1
   cd "%cwd%"
 )
 
