2be9d00d8b26e96451650471e6877af506a9fecf
M. George Hansen <mgeorge@technopolitica.com>
2020-06-26 21:56:07 -0700

    chore(ci): fix logic bug in windows script

--- scripts/install-deps.bat
+++ scripts/install-deps.bat
@@ -6,7 +6,7 @@ if not defined VCPKG_HOME set "VCPKG_HOME=%USERPROFILE%\vcpkg"
 set VCPKG="%VCPKG_HOME%\vcpkg.exe"
 echo "using VCPKG_HOME=%VCPKG_HOME%"
 
-if exist %VCPKG% (
+if not exist %VCPKG% (
   echo "vcpkg not found at $VCPKG, fetching vcpkg and building..."
   set cwd="%cd%"
   git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "%VCPKG_HOME%" || exit /b 1
