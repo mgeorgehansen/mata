f52808d1fa40a9a46ad0f000cb874bdf6ec2e831
M. George Hansen <mgeorge@technopolitica.com>
2020-06-27 11:34:24 -0700

    chore(ci): use debug instead of realease mode during testing

--- scripts/build.bat
+++ scripts/build.bat
@@ -4,4 +4,4 @@ if not exist "build" mkdir build
 cd build
 cmake -G "Visual Studio 15 2017" -DCMAKE_TOOLCHAIN_FILE="%VCPKG_HOME%\scripts\buildsystems\vcpkg.cmake" .. || exit /b 1
 cmake --build . || exit \b 1
-ctest -C Release || exit \b 1
+ctest -C Debug || exit \b 1
