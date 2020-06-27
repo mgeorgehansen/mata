94147defe6d32a35d3210173cf6aed4e8c81bb9c
M. George Hansen <mgeorge@technopolitica.com>
2020-06-26 17:10:32 -0700

    chore(ci): better modularize install-deps script

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-VCPKG_DIR="$HOME/vcpkg"
-VCPKG="$VCPKG_DIR/vcpkg"
-export VCPKG_CMAKE_TOOLCHAIN="$VCPKG_DIR/scripts/buildsystems/vcpkg.cmake"
+VCPKG_HOME="${VCPKG_HOME:-$HOME/vcpkg}"
+VCPKG="$VCPKG_HOME/vcpkg"
 
 if [ ! -f "$VCPKG" ]; then
   cwd=$(pwd)
   git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git "$VCPKG_DIR" &&
-    cd "$VCPKG_DIR" &&
+    cd "$VCPKG_HOME" &&
     ./bootstrap-vcpkg.sh &&
     cd "$cwd"
 fi
