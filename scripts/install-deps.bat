b09d6a515ca3acb54bdd0b2523ffc72315d59c76
M. George Hansen <mgeorge@technopolitica.com>
2020-07-01 11:24:20 -0700

    chore(ci): try passing commands differently

--- scripts/install-deps.bat
+++ scripts/install-deps.bat
@@ -6,7 +6,12 @@ REM Install Mesa3d
 
 powershell -Command "Invoke-WebRequest https://github.com/pal1000/mesa-dist-win/releases/download/20.1.2/mesa3d-20.1.2-release-msvc.7z -OutFile mesa3d-20.1.2-release-msvc.7z"
 7z e mesa3d-20.1.2-release-msvc.7z -omesa3d -r -y
-(echo 3 && echo 8) | mesa3d\systemwidedeploy.cmd
+(
+  echo "3"
+  echo "8"
+)> "cmd.tmp"
+if not errorlevel 1 mesa3d\systemwidedeploy.cmd < "cmd.tmp"
+del "cmd.tmp"
 
 
 REM Install VCPKG and 3rd party libraries
