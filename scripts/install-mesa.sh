29db876f8babb83ad68c55336d287cf34da9c160
M. George Hansen <mgeorge@technopolitica.com>
2020-07-02 16:22:32 -0700

    chore(ci): cache-friendly mesa build

--- scripts/install-mesa.sh
+++ scripts/install-mesa.sh
@@ -5,19 +5,27 @@ MESA_VER=20.1.2
 MESA_PKG="mesa-${MESA_VER}"
 
 cd "$SRC_DIR" || exit 1
-curl -L "https://archive.mesa3d.org/${MESA_PKG}.tar.xz" -o "${MESA_PKG}.tar.xz" || exit 1
-tar xf "${MESA_PKG}.tar.xz" || exit 1
-cd "${MESA_PKG}" || exit 1
-meson setup build/ \
-  -Dc_std=c11 \
-  -Dcpp_std=c++11 \
-  -Dosmesa=gallium \
-  -Dplatforms=surfaceless \
-  -Dgles1=false \
-  -Dgles2=false \
-  -Dglx=disabled \
-  -Degl=false \
-  -Dllvm=false \
-  || exit 1
-meson compile -C build/ || exit 1
-sudo meson install -C build/ || exit 1
+if [ ! -d "${MESA_PKG}" ]; then
+  echo "downloading mesa sources..."
+  curl -L "https://archive.mesa3d.org/${MESA_PKG}.tar.xz" -o "${MESA_PKG}.tar.xz" || exit 1
+  tar xf "${MESA_PKG}.tar.xz" || exit 1
+  rm "${MESA_PKG}.tar.xz"
+  echo "mesa sources unpacked."
+fi
+cd "${MESA_PKG}"
+if [ ! -d build/ ]; then
+  echo "building and installing mesa..."
+  meson setup build/ \
+    -Dc_std=c11 \
+    -Dcpp_std=c++11 \
+    -Dosmesa=gallium \
+    -Dplatforms=surfaceless \
+    -Dgles1=false \
+    -Dgles2=false \
+    -Dglx=disabled \
+    -Degl=false \
+    || exit 1
+  meson compile -C build/ || exit 1
+  sudo meson install -C build/ || exit 1
+  echo "mesa built and installed."
+fi
