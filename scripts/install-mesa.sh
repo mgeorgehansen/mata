ff61c4aa5d30fb3391d39f1279b6c1050068181d
M. George Hansen <mgeorge@technopolitica.com>
2020-07-02 15:14:25 -0700

    chore(ci): use c/c++11 std in mesa to fix compile error

--- scripts/install-mesa.sh
+++ scripts/install-mesa.sh
@@ -9,8 +9,8 @@ curl -L "https://archive.mesa3d.org/${MESA_PKG}.tar.xz" -o "${MESA_PKG}.tar.xz" 
 tar xf "${MESA_PKG}.tar.xz" || exit 1
 cd "${MESA_PKG}" || exit 1
 meson setup build/ \
-  -Dc_std=c17 \
-  -Dcpp_std=c++17 \
+  -Dc_std=c11 \
+  -Dcpp_std=c++11 \
   -Dosmesa=gallium \
   -Dplatforms=surfaceless \
   -Dgles1=false \
