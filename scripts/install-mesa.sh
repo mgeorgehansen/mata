d0a5353a7d9f3f508a24d1bd9044e238b76a8957
M. George Hansen <mgeorge@technopolitica.com>
2020-07-02 14:41:00 -0700

    chore(ci): fix mesa build dir name and exit early on error

--- scripts/install-mesa.sh
+++ scripts/install-mesa.sh
@@ -4,10 +4,10 @@ SRC_DIR="$HOME/src"
 MESA_VER=20.1.2
 MESA_PKG="mesa-${MESA_VER}"
 
-cd "$SRC_DIR"
-curl -L "https://archive.mesa3d.org/${MESA_PKG}.tar.xz" -o "${MESA_PKG}.tar.xz"
-tar xf "${MESA_PKG}.tar.xz"
-cd "${MESA_VER}"
+cd "$SRC_DIR" || exit 1
+curl -L "https://archive.mesa3d.org/${MESA_PKG}.tar.xz" -o "${MESA_PKG}.tar.xz" || exit 1
+tar xf "${MESA_PKG}.tar.xz" || exit 1
+cd "${MESA_PKG}" || exit 1
 meson setup build/ \
   -Dc_std=c17 \
   -Dcpp_std=c++17 \
@@ -16,6 +16,7 @@ meson setup build/ \
   -Dgles1=false \
   -Dgles2=false \
   -Dglx=disabled \
-  -Degl=false
-meson compile -C build/
-sudo meson install -C build/
+  -Degl=false \
+  || exit 1
+meson compile -C build/ || exit 1
+sudo meson install -C build/ || exit 1
