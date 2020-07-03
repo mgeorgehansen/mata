f6d8d078158395c5c619e9ebc095d486b1e35ede
M. George Hansen <mgeorge@technopolitica.com>
2020-07-02 22:17:16 -0700

    test: generate resoureces path at config time for smoke test

--- tests/smoke.cpp
+++ tests/smoke.cpp
@@ -3,45 +3,19 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 #include <stdexcept>
-#define CATCH_CONFIG_RUNNER
+#define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <mata/lib.hpp>
 #include <mata/utils/exceptions.hpp>
 
-static auto resourcesPath = std::string{};
-static auto headless = false;
-
-int main(int argc, char *argv[]) {
-  auto session = Catch::Session{};
-
-  using namespace Catch::clara;
-  auto cli = session.cli() |
-             Opt(resourcesPath, "resourcesPath")["--resourcesPath"](
-                 "path to the resources folder") |
-             Opt(headless)["--headless"](
-                 "whether to run in headless mode (e.g. in CI)");
-  session.cli(cli);
-
-  // Parse command line args.
-  const auto returnCode = session.applyCommandLine(argc, argv);
-  if (0 != returnCode) {
-
-    return returnCode;
-  }
-  if (resourcesPath.compare("") == 0) {
-    std::cerr << "must specify --resourecesPath param";
-    return EXIT_FAILURE;
-  }
-
-  return session.run();
-}
+#include "config.hpp"
 
 TEST_CASE("Smoke test", "[main]") {
   auto params = mata::AppParams{};
-  params.headless = headless;
-  params.resourcesPath = resourcesPath;
+  params.headless = true;
+  params.resourcesPath = MATA_RESOURCES_PATH;
   try {
     const auto app = mata::App(params);
     app.stepFrame();
