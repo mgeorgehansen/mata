cacee18c56e5e5d330f96a55bfbe8755c38663a9
M. George Hansen <mgeorge@technopolitica.com>
2020-07-03 09:05:33 -0700

    chore(ci): add better error handling to install-deps.bat

--- scripts/install-deps.bat
+++ scripts/install-deps.bat
@@ -4,21 +4,25 @@ if not defined VCPKG_HOME set "VCPKG_HOME=%USERPROFILE%\vcpkg"
 set VCPKG="%VCPKG_HOME%\vcpkg.exe"
 echo "using VCPKG_HOME=%VCPKG_HOME%"
 
-set cwd="%cd%"
-cd "%VCPKG_HOME%"
+if not exist "%VCPKG_HOME%" mkdir "%VCPKG_HOME%"
+
+pushd "%VCPKG_HOME%"
 
 git rev-parse --is-inside-work-tree
 if errorlevel 1 (
   echo "vcpkg source not found, cloning vcpkg..."
-  git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git . || exit /b 1
+  git clone --depth=1 --branch 2020.06 https://github.com/Microsoft/vcpkg.git . || ^
+    echo "failed to clone vcpkg source" && exit /b 1
 )
 
 if not exist %VCPKG% (
   echo "vcpkg binary not found at %VCPKG%, building vcpkg..."
-  bootstrap-vcpkg.bat || exit /b 1
+  bootstrap-vcpkg.bat || ^
+    echo "failed to build vcpkg" && exit /b 1
 )
 
-cd "%cwd%" || exit /b 1
+popd
 
 echo "installing deps..."
-%VCPKG% install fmt glfw3 glbinding catch2 || exit /b 1
+%VCPKG% install fmt glfw3 glbinding catch2 || ^
+  echo "failed to install dependencies" && exit /b 1
