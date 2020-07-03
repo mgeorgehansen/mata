c85ad158a8d928ab81d3203a1b9051f8059602f3
M. George Hansen <mgeorge@technopolitica.com>
2020-07-03 09:17:05 -0700

    chore(ci): always install mesa libs

--- scripts/install-mesa.sh
+++ scripts/install-mesa.sh
@@ -4,7 +4,11 @@ SRC_DIR="${SRC_DIR:-$HOME/src}"
 MESA_VER=20.1.2
 MESA_PKG="mesa-${MESA_VER}"
 
-cd "$SRC_DIR" || exit 1
+if [ ! -d "$SRC_DIR" ]; then
+  mkdir -p "$SRC_DIR"
+fi
+cd "$SRC_DIR"
+
 if [ ! -d "${MESA_PKG}" ]; then
   echo "downloading mesa sources..."
   curl -L "https://archive.mesa3d.org/${MESA_PKG}.tar.xz" -o "${MESA_PKG}.tar.xz" || exit 1
@@ -12,9 +16,10 @@ if [ ! -d "${MESA_PKG}" ]; then
   rm "${MESA_PKG}.tar.xz"
   echo "mesa sources unpacked."
 fi
+
 cd "${MESA_PKG}"
 if [ ! -d build/ ]; then
-  echo "building and installing mesa..."
+  echo "building mesa..."
   meson setup build/ \
     -Dc_std=c11 \
     -Dcpp_std=c++11 \
@@ -26,6 +31,7 @@ if [ ! -d build/ ]; then
     -Degl=false \
     || exit 1
   meson compile -C build/ || exit 1
-  sudo meson install -C build/ || exit 1
-  echo "mesa built and installed."
+  echo "mesa built."
 fi
+
+sudo meson install -C build/ || exit 1
